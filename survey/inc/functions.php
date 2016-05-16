c8eb7181d90e6b014a3d9efaa621aa81a42433d4
Christopher Bill <christopher.bill@veda.com.au>
2016-05-16 16:30:39 +1000

    added password generator

---
+++ survey/inc/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function generatePassword($length) {
+
+	$randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+
+	return $randomString;
+
+}
+
+
+?>
--- survey/new_member.php
+++ survey/new_member.php
@@ -4,6 +4,7 @@ date_default_timezone_set("Australia/Melbourne");
 
 //Load config files
 include('inc/config.dev.php');
+include('inc/functions.php');
 //include('inc/config.test.php');
 //include('inc/config.prod.php');
 //Load database class
@@ -16,13 +17,22 @@ $data['page_title'] = "TTM: New Member Form";
 if (isset($_POST['email']) ) {
 //form submitted
 
+	//generate a random password
+	$password = $generatePassword(8);
+	$hashedPassword = md5($password);
+
+	//create INSERT query
+	// TBC
+
+	//set view data
 	$data['heading'] = "Application received!";
 	$data['message'] = "<p>Thank you for joining Transition Towns Maroondah! Your details have been received and we have sent your new password to the email address you provided.</p>";
 	$data['message'] .= "<p>Once, you receive your password you are free to log in and amend your details, if necessary.</p>";
 	$data['message'] .= "<p>Please consider taking part in our <a href='/skills_audit.php'>skills audit</a>. This is a TTM initiative designed to provide us with an idea of our members' skills interest from which we develop our projects.</p>";
 	$data['message'] .= "<h3>Spread the word!</h3>";
 	$data['message'] .= "<a class='twitter-share-button' href='https://twitter.com/intent/tweet?text=I%20just%20joined%20Transition%20Town%20Maroondah' data-hashtags='sustainability,transitiontown'>Tweet</a>";
-
+	
+	//display views
 	require_once('views/header.php');
 	require_once('views/message.php');
 	require_once('views/footer.php');
