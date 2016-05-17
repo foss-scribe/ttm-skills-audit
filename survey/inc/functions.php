682be3937c4bcb433e403bcd08e10a96f90a6d54
Chris Gardiner-Bill <cbill.uk@googlemail.com>
2016-05-17 22:37:48 +1000

    new members are now written to db and a created event note is triggered system prevents duplicate emails being added new password is sent to member's registered email added new resetPassword function added facebook share button to confirmation

--- survey/inc/functions.php
+++ survey/inc/functions.php
@@ -1,10 +1,27 @@
 <?php
 
-function generatePassword($length) {
+function generatePassword($length = 0) {
 
 	$randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
 
 	return $randomString;
+
+}
+
+function resetPassword($email) {
+
+	$newPassword = generatePassword(8);
+	$hashedPassowrd = md5($newPassword);
+
+	$db->query('UPDATE ttm_members SET password = :Password WHERE email = :Email');
+	$db->bind(':Password', $hashedPassowrd);
+	$db->bind(':Email', $email);
+	$db->execute();
+
+
+	$subject = "TTM password reset";
+		$message = "Your TTM password has been reset. Your new password is: $password.";
+		mail($_POST['email'],$subject, $message );
 
 }
 
